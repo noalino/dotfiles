74b93ac8d143d728543b7b8cbed025ad9262dae1
Benoît G <bgelineau.dev@gmail.com>
2022-10-04 16:13:38 +0200

    Add neovim gruvbox colorscheme

---
+++ nvim/lua/user/colorscheme.lua
@@ -0,0 +1,14 @@
+vim.cmd "colorscheme slate"
+
+local colorscheme = "gruvbox"
+
+local success, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+
+if not success then
+  vim.notify("colorscheme " .. colorscheme .. " not found!")
+  return
+end
+
+vim.opt.background = "dark"
+vim.g.gruvbox_contrast_dark = "soft"
+vim.g.gruvbox_contrast_light = "hard"
